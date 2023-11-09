2353d3875ef38c7a71e7d7dbdddbd7c72df55b0b
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-11-09 14:14:03 +0200

    Resolve 01,02,03 tasks

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function( originString ) {
+  let reversedString = '';
+  let originToArray = originString.split( '' );
+  let reverseArray = originToArray.reverse();
+  reversedString = reverseArray.toString().replace( /,/g, '' );
 
+  return reversedString;
 };
 
 // Do not edit below this line
