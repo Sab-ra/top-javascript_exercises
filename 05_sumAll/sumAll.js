a4d19dc6c7f3236866a8cb67588cf6bf52c17221
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-11-13 19:05:22 +0200

    Improve syntaxis in sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,19 @@
 const sumAll = function( a, b ) {
-  if ( typeof a !== 'number' || typeof b !== 'number' ) {
-    return 'ERROR';
-  } else if ( a < 0 || b < 0 ) {
-    return 'ERROR';
-  } else {
-    let startRangeFromNumber = Math.min( a, b );
-    let endRangeWithNumber = Math.max( a, b );
-    let result = 0;
+  if (
+    !Number.isInteger( a ) ||
+    !Number.isInteger( b ) ||
+    a < 0 ||
+    b < 0
+  ) { return 'ERROR'};
 
-    for( let i = startRangeFromNumber; i <= endRangeWithNumber; i++ ) {
-      result += i;
-    }
-    return result;
+  let startRangeFromNumber = Math.min( a, b );
+  let endRangeWithNumber = Math.max( a, b );
+  let result = 0;
+
+  for( let i = startRangeFromNumber; i <= endRangeWithNumber; i++ ) {
+    result += i;
   }
+  return result;
 };
 
 // Do not edit below this line
