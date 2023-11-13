b6c95893565b11df7a71bd7f7cf25df377f0bca4
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-11-13 18:52:11 +0200

    Add all integers in range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function( a, b ) {
+  if ( typeof a !== 'number' || typeof b !== 'number' ) {
+    return 'ERROR';
+  } else if ( a < 0 || b < 0 ) {
+    return 'ERROR';
+  } else {
+    let startRangeFromNumber = Math.min( a, b );
+    let endRangeWithNumber = Math.max( a, b );
+    let result = 0;
 
+    for( let i = startRangeFromNumber; i <= endRangeWithNumber; i++ ) {
+      result += i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
